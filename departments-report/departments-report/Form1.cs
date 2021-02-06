c842ddf321606b46b2e48f5a9e6fd984aa083a04
Marco Vasquez <macroxx120@gmail.com>
2021-02-06 17:27:13 -0500

    saved some changes

--- departments-report/departments-report/Form1.cs
+++ departments-report/departments-report/Form1.cs
@@ -16,5 +16,10 @@ namespace departments_report
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
